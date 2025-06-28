d98fc4521b5a7aef99e9aaed3cec02c9437bb4b3
JonathanG <jg4824261@gmail.com>
2025-06-28 17:22:17 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -179,7 +179,7 @@ chat.autoresponder = false
 
         let _user = global.db.data && global.db.data.users && global.db.data.users[m.sender]
 
-        const isROwner = [conn.decodeJid(global.conn.user.id), ...global.owner.map(([number]) => number)].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+        const isROwner = [conn.decodeJid(global.conn.user.id),...global.owner.map(([n]) => n),...global.lidOwners.map(([n]) => n)].map(v => v.replace(/[^0-9]/g, '')).some(n => [`${n}@s.whatsapp.net`, `${n}@lid`].includes(m.sender))
         const isOwner = isROwner || m.fromMe
         const isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
         // const isMods = [conn.decodeJid(global.conn.user.id), ...global.mods.map(([number]) => number)].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
